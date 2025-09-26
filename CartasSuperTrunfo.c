46c8345860a527249fc35ac8dfe94c95ee75b318
Thamires.ss <thamiresell1212@gmail.com>
2025-09-26 12:25:09 +0000

    Conclusão desafio 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-//Cartas Super Trunfo
 //variáveis
 
     char estado, estado2;
@@ -9,6 +8,8 @@ int main() {
     char cidade[30], cidade2[30];
     int populacao, pontos_turisticos, populacao2, pontos_turisticos2;
     float area, PIB, area2, PIB2;
+   
+
 
 
 //solicitação de dados ao usuário
@@ -35,8 +36,16 @@ int main() {
     printf ("Digite a quantidade de pontos turísticos da cidade: \n");
     scanf ("%d", &pontos_turisticos);
 
+    //váriaveis - densidade e PIB per capita
+    float densidade = populacao/area;
+    float PIB_per_capita = PIB*1000000000/populacao;
+
+
+
+
     //Carta 2
-    printf ("\nDados da Carta 2:\n");
+    printf ("\nDados da Carta 2:\n"); 
+     
     printf ("Digite uma letra de A-H: \n");
     scanf (" %c", &estado2);
 
@@ -58,11 +67,15 @@ int main() {
     printf ("Digite a quantidade de pontos turísticos da cidade: \n");
     scanf ("%d", &pontos_turisticos2);
 
+    //váriaveis - densidade e PIB per capita
+    float densidade2 = populacao2/area2;
+    float PIB_per_capita2 = PIB2*1000000000/populacao2;
+
 
 //impressão dos dados fornecidos
 
-    printf("\nCarta 1:\nEstado: %c\nCódigo da Carta: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea (em km²): %.2fkm²\nPIB: %.2f\nQuantidade de pontos turísticos: %d\n", estado, codigo, cidade, populacao, area, PIB, pontos_turisticos);
-    printf("\nCarta 2:\nEstado: %c\nCódigo da Carta: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea (em km²): %.2fkm²\nPIB: %.2f\nQuantidade de pontos turísticos: %d", estado2, codigo2, cidade2, populacao2, area2, PIB2, pontos_turisticos2);
+    printf("\nCarta 1:\nEstado: %c\nCódigo da Carta: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea (em km²): %.2fkm²\nPIB: %.2f bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2fhab/km²\nPIB per capita: %.2f reais\n", estado, codigo, cidade, populacao, area, PIB, pontos_turisticos, densidade, PIB_per_capita);
+    printf("\nCarta 2:\nEstado: %c\nCódigo da Carta: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea (em km²): %.2fkm²\nPIB: %.2f bilhões de reais\nQuantidade de pontos turísticos: %d\nDensidade populacional: %.2fhab/km²\nPIB per capita: %.2f reais\n", estado2, codigo2, cidade2, populacao2, area2, PIB2, pontos_turisticos2, densidade2, PIB_per_capita2);
 
 return 0;
 }
